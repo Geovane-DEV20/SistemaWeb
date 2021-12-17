0c698b13395be464f7b31b11c117fb23d189762c
Geovane <geovaneDEV1998@gmail.com>
2021-12-16 22:46:47 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecords.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecords
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Selle { get; set; }   
+
+        public SalesRecords()
+        {
+
+        }
+
+        public SalesRecords(int id, DateTime date, double amount, SalesStatus status, Seller selle)
+        {
+            Id = id;
+            Date = date;
+            this.amount = amount;
+            Status = status;
+            Selle = selle;
+        }
+    }
+}
